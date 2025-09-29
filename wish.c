c0f687af05ca7c5d419362ccc67eed2ecf261c56
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2025-09-29 09:46:03 -0500

    Implement interactive input loop in wish.c

--- wish.c
+++ wish.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <errno.h> // Error handling library. Assigns errno variable with error code when they occur.
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    char inputBuffer[100];
+    int exit = 0;
+    while (!exit) { // Infinite loop to continuously prompt for input
+        // Prompt user for input
+        printf("wish>");
+        if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != NULL) { // up to 99 chars + null terminator
+            printf("You entered: %s", inputBuffer);
+        } else {
+            // Handle error
+            printf("Command not recognised, please try again.\n");
+            printf("DEBUG: Value of errno: %d\n", errno); 
+        }
+        inputBuffer[0] = '\0'; // Clear buffer by setting first character to null terminator
+    }
     return 0;
 }
