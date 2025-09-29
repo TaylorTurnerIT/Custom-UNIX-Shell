dd30402a6fd67bd365e820d7722410cb855a4a69
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2025-09-29 09:47:29 -0500

    Add detailed errno value descriptions to wish.c

--- wish.c
+++ wish.c
@@ -18,3 +18,28 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
+
+/*
+errno value | Error Description
+------------|------------------
+1 (EPERM)   | Operation not permitted
+2 (ENOENT)  | No such file or directory
+3 (ESRCH)   | No such process
+4 (EINTR)   | Interrupted system call
+5 (EIO)     | I/O error
+6 (ENXIO)   | No such device or address
+7 (E2BIG)   | Argument list too long
+8 (ENOEXEC) | Exec format error
+9 (EBADF)   | Bad file number
+10 (ECHILD) | No child processes
+11 (EAGAIN) | Try again
+12 (ENOMEM) | Out of memory
+13 (EACCES) | Permission denied
+14 (EFAULT) | Bad address
+15 (ENOTBLK)| Block device required
+16 (EBUSY)  | Device or resource busy
+17 (EEXIST) | File exists
+18 (EXDEV)  | Cross-device link
+19 (ENODEV) | No such device
+20 (ENOTDIR)| Not a directory
+*/
